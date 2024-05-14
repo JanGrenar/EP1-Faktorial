95f0c97641db231c510b834695ea8cfcd61482e0
Jan Grénar <Grenar.Jan@stud.vassboskovice.cz>
2024-05-14 13:32:17 +0200

    6. úkol

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -1,7 +1,19 @@
 Imports System
+Imports System.Net.Sockets
 
 Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+    Sub Main(args As String())
+        Dim cislo As Integer
+        Dim soucet As Integer
+
+        Console.WriteLine("Zadejte èíslo")
+        cislo = Console.ReadLine
+
+        For x = 0 To cislo
+            soucet = soucet + x
+        Next
+
+        Console.WriteLine($"Souèet èísel od 0 do {cislo} je {soucet}")
+
+    End Sub
 End Module
